a07801e550d30bfcc24a082ca5d25285c5b53714
zohac <simon@jouan.ovh>
2020-07-29 15:10:26 +0200

    [feature][factorization]: install rector for correction

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -27,7 +27,7 @@ class User implements AdvancedUserInterface, EntityInterface
      *
      * @ORM\Id()
      * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer", options={"unsigned": true})
+     * @ORM\Column(type="integer", options={"unsigned"=true})
      */
     private $id;
 
