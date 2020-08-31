7df2ffe30c680748672cdadd386bc9b201cb8698
zohac <simon@jouan.ovh>
2020-08-31 22:21:36 +0200

    [feature][roles]: php-cs-fixer correction

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -35,8 +35,7 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
-        throw new \LogicException(
-            'This method can be blank - it will be intercepted by the logout key on your firewall.'
-        );
+        $message = 'This method can be blank - it will be intercepted by the logout key on your firewall.';
+        throw new \LogicException($message);
     }
 }
