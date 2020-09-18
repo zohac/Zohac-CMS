555d962996237d10d7a701eee8d26d370325363d
zohac <simon@jouan.ovh>
2020-09-18 19:34:59 +0200

    [feature][maintenance]: tailwind installation

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: [],
+  prefix: '',
+  corePlugins: {
+    gridTemplateColumns: false,
+    container: false,
+  }
+}
