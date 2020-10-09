8cb9e4ef9e490bf9fe46623fce8ffcfa6365d813
zohac <simon@jouan.ovh>
2020-10-09 13:59:59 +0200

    feature/shell: code climate correction

--- assets/js/shell/index.js
+++ assets/js/shell/index.js
@@ -24,13 +24,8 @@ function addEventListenerOnShell(shell) {
 function moveShell(shell) {
     let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
-    if (shell.header) {
-        /* if present, the header is where you move the DIV from:*/
-        shell.header.onmousedown = dragMouseDown;
-    } else {
-        /* otherwise, move the DIV from anywhere inside the DIV:*/
-        shell.simulator.onmousedown = dragMouseDown;
-    }
+    /* the header is where you move the DIV from:*/
+    shell.header.onmousedown = dragMouseDown;
 
     function dragMouseDown(event) {
         event.preventDefault();
