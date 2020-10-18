722cbcc3eee99d08aab8990c2c93feda3c5e3075
zohac <simon@jouan.ovh>
2020-10-18 09:58:53 +0200

    feature/menu: update the menu

---
+++ src/Form/MenuItemType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\MenuItem;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MenuItemType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('uuid')
+            ->add('archived')
+            ->add('link')
+            ->add('menuItem')
+            ->add('menu')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => MenuItem::class,
+        ]);
+    }
+}
