7443cbbae09e053eb08a971f61f15c171e0a4232
zohac <simon@jouan.ovh>
2020-12-22 14:12:46 +0100

    Revert "feature/cv: update github actions"
    This reverts commit 706873ad

---
+++ tests/Service/ViewServiceTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Interfaces\Event\EventInterface;
+use App\Service\ViewService;
+use PHPUnit\Framework\Exception;
+use PHPUnit\Framework\ExpectationFailedException;
+use SebastianBergmann\RecursionContext\InvalidArgumentException;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ViewServiceTest extends KernelTestCase
+{
+    /**
+     * @var ViewService
+     */
+    private $viewService;
+
+    public function setUp(): void
+    {
+        self::bootKernel(['debug' => 0]);
+        $this->viewService = self::$container->get(ViewService::class);
+    }
+
+    /**
+     * @throws Exception
+     * @throws ExpectationFailedException
+     * @throws InvalidArgumentException
+     */
+    public function testSetData()
+    {
+        $return = $this->viewService->setData('view', ['an option']);
+        $this->assertInstanceOf(ViewService::class, $return);
+
+        $this->assertEquals('view', $this->viewService->getView());
+        $this->assertTrue(is_array($this->viewService->getOptions()));
+    }
+
+    /**
+     * @throws Exception
+     * @throws ExpectationFailedException
+     * @throws InvalidArgumentException
+     */
+    public function testSetView()
+    {
+        $return = $this->viewService->setData('view', ['an option']);
+        $this->assertInstanceOf(ViewService::class, $return);
+
+        $return = $this->viewService->setView('view-2');
+        $this->assertInstanceOf(ViewService::class, $return);
+
+        $this->assertEquals('view-2', $this->viewService->getView());
+    }
+
+    /**
+     * @throws Exception
+     * @throws ExpectationFailedException
+     * @throws InvalidArgumentException
+     */
+    public function testAddOptions()
+    {
+        $return = $this->viewService->setData('view', ['an option']);
+        $this->assertInstanceOf(ViewService::class, $return);
+
+        $return = $this->viewService->addOptions(['an another option']);
+        $this->assertInstanceOf(ViewService::class, $return);
+
+        $this->assertContains('an option', $this->viewService->getOptions());
+        $this->assertContains('an another option', $this->viewService->getOptions());
+    }
+
+    /**
+     * @throws Exception
+     * @throws ExpectationFailedException
+     * @throws InvalidArgumentException
+     */
+    public function testGetViewEvents()
+    {
+        $events = $this->viewService->getViewEvents();
+
+        foreach ($events as $event) {
+            $this->assertInstanceOf(EventInterface::class, $event);
+        }
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        // avoid memory leaks
+        $this->viewService = null;
+    }
+}
